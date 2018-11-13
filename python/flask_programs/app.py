b6bbb620a45b4f6214d2eead234a7ed479380f0b
ARJUN <arjun1709@gmail.com>
2018-11-13 16:22:32 +0530

    python flask

--- python/flask_programs/app.py
+++ python/flask_programs/app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app=Flask(__name__)     #__name__ is a special characher in Flask
+app=Flask(__name__)     #__name__ is a special character in Flask
+
+@app.route('/send', methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getName=request.form['name']
+        getMail=request.form['mail']
+        getNumber=request.form['number']
+        getSubject=request.form['subject']
+        getMessage=request.form['message']
+        return render_template('result.html',newName=getName, newMail=getMail, newNumber=getNumber, newSubject=getSubject, newMessage=getMessage)
 
 @app.route('/')
-def index():
-    return 'WELCOME TO FLASK....'
-
-@app.route('/home')
 def home():
-    return 'Welcome To Home'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contactus')
+def contact():
+    return render_template('contactus.html')
 
 if(__name__=='__main__'):
     app.run(debug=True)
